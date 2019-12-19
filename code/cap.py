c42e68ad14274fd13996a47edee9fa648a5e4c80
sarut hansombat(toey) <52145750+belmeo03@users.noreply.github.com>
2019-12-20 00:50:18 +0700

    GUI

--- code/cap.py
+++ code/cap.py
@@ -1,17 +1,54 @@
-
-# import the necessary packages
-from imutils.video import FPS
-import numpy as np
-import argparse
-import imutils
 import cv2
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", required=True,
-	help="path to input video file")
-args = vars(ap.parse_args())
- 
-# open a pointer to the video stream and start the FPS timer
-stream = cv2.VideoCapture(args["video"])
-fps = FPS().start()
+import numpy as np
+
+def frame_cap():
+	cam = cv2.VideoCapture(0)
+	cv2.namedWindow("test")
+	img_counter = 0
+
+
+	while True:
+		ret, frame = cam.read()
+		ret, fr = cam.read()
+
+		cv2.line(fr , (0, 220), (800, 220), (255, 0, 0), 2)
+		cv2.line(fr, (0, 260), (800, 260), (0, 255, 0), 2)
+
+		cv2.line(fr, (290, 0), (290, 800), (255, 0, 0), 2)
+		cv2.line(fr, (335, 0), (335, 800), (0, 255, 0), 2)
+		cv2.imshow("test", fr)
+
+		gray1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		th1 = cv2.adaptiveThreshold(gray1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 40)
+		cv2.imshow("test1", th1) #เอาไว้ดูวิดิโอที่เป็น binary
+
+		if not ret:
+			break
+
+		k = cv2.waitKey(1)
+
+		if k%256 == 27:
+			# ESC pressed
+			print("Escape hit, closing...")
+			break
+
+
+		elif k%256 == ord('z'):
+			img_name = "opencv_frame_{}.png"
+			cv2.imwrite(img_name, frame)
+			img = cv2.imread(img_name)
+			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			th = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 40)
+			#Imgcut = th[220:260, 290:335]
+			cv2.imwrite("BinaryS21.png", th)
+			cv2.imshow('Adaptive threshold', th)
+			#print("{} written!".format(img_name))
+			img_counter += 1
+
+
+
+	cam.release()
+
+	cv2.destroyAllWindows()
+
+frame_cap()
--- code/frame_crop.py
+++ code/frame_crop.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("number/img31.png",0)
+img = cv2.imread("number/img34.png",0)
 # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # winname = "Test"
 # cv2.namedWindow(winname)        # Create a named window
@@ -18,6 +18,7 @@ cols = Height
 
 SumPixel_H = np.array([cols-(y/255) for y in Img_First.sum(axis=1)])
 
+print(SumPixel_H)
 
 
 Spec_HolBegin = 0
@@ -39,17 +40,17 @@ for SpScore in range(Spec_HolBegin,Hol,+1):
       Spec_HolEnd=+SpScore
       break
 UpperAndLower_Cut = Img_First[Spec_HolBegin : Spec_HolEnd,0:Width]
-cv2.imwrite("code/frame/frame_ver"+'.png',UpperAndLower_Cut)
-# winname = "Test"
-# cv2.namedWindow(winname)        # Create a named window
-# cv2.moveWindow(winname, 40,30)  # Move it to (40,30)
-# cv2.imshow(winname, UpperAndLower_Cut)
-# cv2.waitKey()
-# cv2.destroyAllWindows()
-# print(Spec_HolEnd)
+# cv2.imwrite("frame/frame_ver"+'.png',UpperAndLower_Cut)
+winname = "Test"
+cv2.namedWindow(winname)        # Create a named window
+cv2.moveWindow(winname, 40,30)  # Move it to (40,30)
+cv2.imshow(winname, UpperAndLower_Cut)
+cv2.waitKey()
+cv2.destroyAllWindows()
+print(Spec_HolEnd)
 
-Height3,Width3 = UpperAndLower_Cut.shape
-rows3=Height3
-cols3=Width3
-SumPixel_Ver = np.array([rows3-(y/255) for y in UpperAndLower_Cut.sum(axis=0)])#หาผลรวมของpixelสีดำในแนวcol
-print(SumPixel_Ver)
+# Height3,Width3 = UpperAndLower_Cut.shape
+# rows3=Height3
+# cols3=Width3
+# SumPixel_Ver = np.array([rows3-(y/255) for y in UpperAndLower_Cut.sum(axis=0)])#หาผลรวมของpixelสีดำในแนวcol
+# print(SumPixel_Ver)
